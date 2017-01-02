f2205ffd98bfffd83f8d592c22849e3ff5436274
mujunKim <mjom5144@gmail.com>
2017-01-02 17:24:58 +0900

    my first ejs2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/set/count', function(req, res){
   res.render('my_first_ejs', data);
 });
 app.get('/set/:num', function(req, res){
-  data.count=req.param.num;
+  data.count=req.params.num;
   res.render('my_first_ejs', data);
 });
 
