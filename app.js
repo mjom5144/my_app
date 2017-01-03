d8833c16910f31e98c5209c84c6b3d0294bc7bc1
mujunKim <mjom5144@gmail.com>
2017-01-03 14:30:49 +0900

    board (Back-end) without login

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// import modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+// create database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function(){
@@ -12,67 +15,63 @@ db.on("error", function(){
   console.log("DB ERROR : ", err);
 });
 
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+// set models
+var postSchema = mongoose.Schema({
+  title : {type:String, required:true}, //제목
+  body : {type:String, required:true}, //본문
+  createdAt : {type:Date, default:Date.now}, //생성시간
+  updatedAt : Date //수정시간
 });
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"}, function(err,data){
-  if(err) return console.log("Data ERROR : ", err);
-  if(!data){
-    Data.create({name:"myData", count:0}, function(err,data){
-      if(err) return console.log("Data ERROR : ", err);
-      console.log("Counter initialized : ", data);
-    });
-  }
-  else console.log("myData already exist");
-});
+var Post = mongoose.model('post', postSchema);
 
+// set views
 app.set("view engine", 'ejs');
+
+// set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
-app.get('/', function(req,res){
-  Data.findOne({name:"myData"}, function(err, data){
-    if(err) return console.log("Data ERROR :", err);
-    data.count++;
-    data.save(function (err){
-      if(err) return console.log("Data ERROR : ", err);
-      res.render('my_first_ejs', data);
-    });
+// set routes
+// index
+app.get('/posts', function(req, res){
+  Post.find({}, function(err, posts){
+    //에러이면 success에 false값 대입하여 제이슨 형성 후 리턴
+    if(err) return res.json({success:false, message:err});
+    //에러가 아니면 success에 true, data에 posts값 대입하여 제이슨 형성
+    res.json({success:true, data:posts});
   });
 });
-app.get('/reset', function(req,res){
-  setCounter(res, 0);
-});
-app.get('/set/count', function(req, res){
-  if(req.query.count) setCounter(res, req.query.count);
-  else getCounter(res);
-});
-app.get('/set/:num', function(req, res){
-  if(req.params.num) setCounter(res, req.params.num);
-  else getCounter(res);
-});
-
-function setCounter(res, num){
-  console.log("setCounter");
-  Data.findOne({name:"myData"}, function(err,data){
-    if(err) return console.log("Data ERROR : ", err);
-    data.count = num;
-    data.save(function (err){
-      if(err) return console.log("Data ERROR : ", err);
-      res.render('my_first_ejs', data);
-    });
+// create
+app.post('/posts', function(req, res){
+  Post.create(req.body.post, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
   });
-}
-
-function getCounter(res){
-  console.log("getCounter");
-  Data.findOne({name:"myData"}, function(err,data){
-    if(err) return console.log("Data ERROR : ", err);
-    res.render('my_first_ejs', data);
+});
+// show
+app.get('/posts/:id', function(req, res){
+  Post.findById(req.params.id, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
   });
-}
+});
+// update
+app.put('/posts/:id', function(req, res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" updated"});    
+  });
+});
+//destroy
+app.delete('/posts/:id', function(req, res){
+  Post.findByIdAndRemove(req.params.id, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id + " deleted"});
+  });
+});
 
+// start server
 app.listen(3000, function(){
   console.log('Server On!');
 });
